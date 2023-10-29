440594f881fc56bb5a15e71fd9497ca8d13f10db
Sal0209 <tuscanosally2@gmail.com>
2023-10-29 21:43:04 +0530

    Update +page.svelte
    image border

--- src/routes/(tools)/basic-photo-editor/+page.svelte
+++ src/routes/(tools)/basic-photo-editor/+page.svelte
@@ -12,8 +12,18 @@
   let editedImageSrc = null;
   export let data;
 
+      image.onload = () => {
+        if(canvas){
+        //const canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        const ctx = canvas.getContext('2d');
 
-  
+        if (ctx) {
+          ctx.drawImage(image, 0, 0);
+          // Set the border size and color
+          ctx.lineWidth = $borderSize;
+          ctx.strokeStyle = $borderColor;
 
   function handleFileUpload(event) {
     const uploadedFile = event.target.files[0];
@@ -22,16 +32,106 @@
     }
   }
 
+          // Set the updated image with the border
+          uploadedImage = canvas.toDataURL();
+        }
+      }
+    };
+    image.src= uploadedImage;
+  }
+}
+async function handleFileInput( event ) {
+    const file = event.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      const image = new Image();
+      image.src = imageUrl;
+      await image.decode(); // Ensure the image is loaded
+      
+      // Get a reference to the canvas element
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      if(ctx){
+      // Set canvas dimensions to match the image
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.drawImage(image, 0, 0);
 
+      ctx.lineWidth = $borderSize;
+      ctx.strokeStyle = $borderColor;
+      ctx.strokeRect(0, 0, canvas.width, canvas.height);
+      uploadedImage = canvas.toDataURL();
 
-</script>
+      URL.revokeObjectURL(imageUrl); // Clean up the object URL
+      //applyBorder();
+    }
+  } 
+}
+// Function to handle changes in border size
+function handleBorderSize(event) {
+    $borderSize = event.target.value;
+  }
 
+  // Function to handle changes in border color
+  function handleBorderColor(event) {
+    $borderColor = event.target.value;
+  }
+  
+  function downloadImage() {
+    if (uploadedImage) {
+      const link = document.createElement('a');
+      link.href = uploadedImage;
+      link.download = 'bordered_image.png';
+      link.click();
+    }
+  }
+  /*function toggleBackground() {
+    backgroundColor.update((currentColor) =>
+      currentColor === '#FFFFFF' ? '#000000' : '#FFFFFF'
+    );
+  }*/
 
   <Intro heading={data.meta.title} description={data.meta.description} />
 
+<Intro heading={data.meta.title} description={data.meta.description} />
 
+<main>
+	<h1 class="text-white">Image Bordered Effect</h1>
   
-
+	<input
+	  type="file"
+	  accept="image/*"
+	  on:change={handleFileInput}
+	  class="text-white"
+    style="width: 500px"
+	/>
+  
+	<label class="text-white">
+	  Bordered Size:
+	  <input
+		type="range"
+		min="1"
+		max="50"
+		step="1"
+		bind:value={$borderSize}
+	  />
+	  {$borderSize} pixels
+	</label>
+  <div>
+  <label class="text-white" >
+	  Bordered Color:
+	  <input
+		type="color"
+		bind:value={$borderColor}
+	  />
+	  {$borderColor}
+	</label>
+  </div>
+  
+  
+	{#if uploadedImage}
+	<div class="image-container">
+		<h2 class="text-white"><b>BORDERED IMAGE</b></h2>
     
       <Label for="with_helper" class="pb-2">Upload file</Label>
       <Fileupload id="with_helper" class="mb-2" on:change={handleFileUpload} />
